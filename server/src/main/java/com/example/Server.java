2d4404d8a47a6740e4d866e1bb301712ecdca2af
Roham <Roham8406@gmail.com>
2025-12-07 22:31:35 +0330

    3

--- server/src/main/java/com/example/Server.java
+++ server/src/main/java/com/example/Server.java
@@ -1,72 +1,10 @@
 package com.example;
 
-import org.java_websocket.server.WebSocketServer;
-import org.java_websocket.handshake.ClientHandshake;
-import org.java_websocket.WebSocket;
-import org.java_websocket.server.DefaultSSLWebSocketServerFactory;
+import io.github.some_example_name.server.model.GameThread;
 
-import javax.net.ssl.*;
-import java.io.FileInputStream;
-import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
-import java.security.KeyStore;
+public class Server {
 
-public class Server extends WebSocketServer {
-
-    public Server(int port, SSLContext sslContext) {
-        super(new InetSocketAddress(port));
-        this.setWebSocketFactory(new DefaultSSLWebSocketServerFactory(sslContext));
-    }
-
-    @Override
-    public void onOpen(WebSocket conn, ClientHandshake handshake) {
-        System.out.println("Client connected: " + conn.getRemoteSocketAddress());
-    }
-
-    @Override
-    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
-        System.out.println("Client disconnected.");
-    }
-
-    @Override
-    public void onMessage(WebSocket webSocket, String s) {
-
-    }
-
-    @Override
-    public void onMessage(WebSocket conn, ByteBuffer message) {
-        broadcast(message.array());
-        System.out.println("Broadcast " + message.array().length + " bytes");
-    }
-
-    @Override
-    public void onError(WebSocket conn, Exception ex) {
-        ex.printStackTrace();
-    }
-
-    @Override
-    public void onStart() {
-        System.out.println("WSS server started on port 8443");
-    }
-
-    public static SSLContext loadSSL() throws Exception {
-        char[] pass = "password".toCharArray();
-
-        KeyStore ks = KeyStore.getInstance("JKS");
-        ks.load(new FileInputStream("keystore.jks"), pass);
-
-        KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
-        kmf.init(ks, pass);
-
-        SSLContext ssl = SSLContext.getInstance("TLS");
-        ssl.init(kmf.getKeyManagers(), null, null);
-        return ssl;
-    }
-
-    public static void main(String[] args) throws Exception {
-        SSLContext ssl = loadSSL();
-        Server server = new Server(8443, ssl);
-        server.start();
-        System.out.println("Server running with WSS (TLS).");
+    public static void main(String[] args) {
+        GameThread.getInstance().start();
     }
 }
