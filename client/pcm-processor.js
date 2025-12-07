4fe23707ea917a25977d8aa5cc36bccd9de87ed1
Roham <Roham8406@gmail.com>
2025-12-08 02:11:25 +0330

    improving the sound

--- client/pcm-processor.js
+++ client/pcm-processor.js
@@ -5,6 +5,7 @@ class PCMProcessor extends AudioWorkletProcessor {
             const buffer = new ArrayBuffer(input.length * 2);
             const view = new DataView(buffer);
             for (let i = 0; i < input.length; i++) {
+                // Clip audio to [-1,1] to reduce distortion
                 let s = Math.max(-1, Math.min(1, input[i]));
                 view.setInt16(i*2, s*0x7fff, true);
             }
